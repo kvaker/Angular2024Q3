c25ad00d8b7e65914786f5778688b24151a5e5ad
v-kuntsevich <kwakin@gmail.com>
2024-07-28 17:53:19 +0300

    feat: remove the Search button, create Observable(3 characters), add debounce(if still typing)

--- YouTube-client-app/src/app/core/components/header/header.component.ts
+++ YouTube-client-app/src/app/core/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Output } from "@angular/core";
+import {
+    Component, EventEmitter, OnDestroy, OnInit, Output
+} from "@angular/core";
+import { Subject, Subscription } from "rxjs";
+import { debounceTime, distinctUntilChanged, filter } from "rxjs/operators";
 
 import { AuthService } from "../../../auth/services/auth.service";
 
@@ -10,12 +14,31 @@ import { AuthService } from "../../../auth/services/auth.service";
     templateUrl: "./header.component.html",
     styleUrl: "./header.component.scss",
 })
-export class HeaderComponent {
-    constructor(private authService: AuthService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+    private searchTerms = new Subject<string>();
+    private searchSubscription!: Subscription;
+    showBottomHeader: boolean = false;
 
     @Output() sortByChanged = new EventEmitter<string>();
     @Output() searchTermChanged: EventEmitter<string> = new EventEmitter<string>();
-    showBottomHeader: boolean = false;
+
+    constructor(private authService: AuthService) {}
+
+    ngOnInit() {
+        this.searchSubscription = this.searchTerms.pipe(
+            debounceTime(300),
+            filter((term) => term.length >= 3),
+            distinctUntilChanged()
+        ).subscribe((term) => {
+            this.searchTermChanged.emit(term);
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.searchSubscription) {
+            this.searchSubscription.unsubscribe();
+        }
+    }
 
     toggleHeaderBottom() {
         this.showBottomHeader = !this.showBottomHeader;
