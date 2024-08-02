f575e19d97ee111e230871da8d9e8fb637b90652
v-kuntsevich <kwakin@gmail.com>
2024-08-02 23:56:27 +0300

    feat: add video reducer

---
+++ YouTube-client-app/src/app/redux/reducers/video.reducer.ts
@@ -0,0 +1,23 @@
+import { createReducer, on } from "@ngrx/store";
+
+import { VideoItem } from "../../youtube/models/video.model";
+import { loadVideos, loadVideosFailure, loadVideosSuccess } from "../actions/video.actions";
+
+export interface VideoState {
+    videos: VideoItem[];
+    loading: boolean;
+    error: string | null;
+}
+
+export const initialState: VideoState = {
+    videos: [],
+    loading: false,
+    error: null
+};
+
+export const videoReducer = createReducer(
+    initialState,
+    on(loadVideos, (state) => ({ ...state, loading: true })),
+    on(loadVideosSuccess, (state, { videos }) => ({ ...state, loading: false, videos })),
+    on(loadVideosFailure, (state, { error }) => ({ ...state, loading: false, error }))
+);
