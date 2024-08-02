aa5cfa001a30037b4bd86ddb71dc8dfbfe8e3b45
v-kuntsevich <kwakin@gmail.com>
2024-08-02 23:53:45 +0300

    feat: add redux video actions

---
+++ YouTube-client-app/src/app/redux/actions/video.actions.ts
@@ -0,0 +1,7 @@
+import { createAction, props } from "@ngrx/store";
+
+import { VideoItem } from "../../youtube/models/video.model";
+
+export const loadVideos = createAction("[Video] Load Videos");
+export const loadVideosSuccess = createAction("[Video] Load Videos Success", props<{ videos: VideoItem[] }>());
+export const loadVideosFailure = createAction("[Video] Load Videos Failure", props<{ error: string }>());
