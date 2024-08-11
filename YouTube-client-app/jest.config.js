ba02c6a7b8a9ef5cbc584fa35c7546b711eafc49
v-kuntsevich <kwakin@gmail.com>
2024-08-11 23:38:38 +0300

    feat: delete Karma and Jasmine, object test from package.json, add Jest and config files

---
+++ YouTube-client-app/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.html$',
+      },
+    },
+    testMatch: ['**/+(*.)+(spec).+(ts)'],
+    transform: {
+      '^.+\\.(ts|js|html)$': 'ts-jest',
+    },
+    transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+    moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+    moduleNameMapper: {
+      '^src/(.*)$': '<rootDir>/src/$1',
+    },
+    collectCoverage: true,
+    coverageReporters: ['html'],
+    coverageDirectory: '<rootDir>/coverage',
+    testEnvironment: 'jsdom',
+  };
+  
