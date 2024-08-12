9c84c0c02286a89a6022c9d2dae418f71bf01faf
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-08-12 16:00:06 +0300

    feat: add test component search-results.component.spec and fix JIT mode Jest config with Ivy and AOT

--- YouTube-client-app/src/app/search/search-results/search-results.component.spec.ts
+++ YouTube-client-app/src/app/search/search-results/search-results.component.spec.ts
@@ -1,22 +1,51 @@
+import { signal } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { SearchResponse } from "../models/search-response.model";
+import { SearchDataService } from "../services/search-data.service";
 import { SearchResultsComponent } from "./search-results.component";
 
 describe("SearchResultsComponent", () => {
     let component: SearchResultsComponent;
     let fixture: ComponentFixture<SearchResultsComponent>;
+    let mockSearchDataService: Partial<SearchDataService>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [SearchResultsComponent],
+    beforeEach(() => {
+        mockSearchDataService = {
+            searchResults$: signal<SearchResponse | null>(null),
+        };
+
+        TestBed.configureTestingModule({
+            declarations: [SearchResultsComponent],
+            providers: [
+                { provide: SearchDataService, useValue: mockSearchDataService },
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(SearchResultsComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
     });
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should have searchResults with 0 items", () => {
+        const searchResponse: SearchResponse = {
+            kind: "youtube#searchListResponse",
+            etag: "some-etag",
+            pageInfo: { totalResults: 0, resultsPerPage: 5 },
+            items: [],
+        };
+
+        if (mockSearchDataService.searchResults$) {
+            mockSearchDataService.searchResults$.set(searchResponse);
+            fixture.detectChanges();
+
+            const searchResults = mockSearchDataService.searchResults$();
+            expect(searchResults?.items.length).toBe(0);
+        } else {
+            fail("mockSearchDataService.searchResults$ is undefined");
+        }
     });
 });
