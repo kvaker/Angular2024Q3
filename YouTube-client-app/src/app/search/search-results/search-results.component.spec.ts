b93bff7155fb85da1cf84ce20167bf0d43ef1561
Vitali Kuntsevich <kvadesstudio@gmail.com>
2024-08-12 17:20:13 +0300

    fix: debug test search-results.component with mockWritableSignal and add aot in angular.json

--- YouTube-client-app/src/app/search/search-results/search-results.component.spec.ts
+++ YouTube-client-app/src/app/search/search-results/search-results.component.spec.ts
@@ -1,9 +1,33 @@
-import { signal } from "@angular/core";
+import { WritableSignal } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SearchResponse } from "../models/search-response.model";
 import { SearchDataService } from "../services/search-data.service";
 import { SearchResultsComponent } from "./search-results.component";
+
+class MockWritableSignal<T> {
+    private value: T;
+
+    constructor(initialValue: T) {
+        this.value = initialValue;
+    }
+
+    set(newValue: T) {
+        this.value = newValue;
+    }
+
+    update(updateFn: (value: T) => T) {
+        this.value = updateFn(this.value);
+    }
+
+    asReadonly() {
+        return () => this.value;
+    }
+
+    call(): T {
+        return this.value;
+    }
+}
 
 describe("SearchResultsComponent", () => {
     let component: SearchResultsComponent;
@@ -11,8 +35,10 @@ describe("SearchResultsComponent", () => {
     let mockSearchDataService: Partial<SearchDataService>;
 
     beforeEach(() => {
+        const searchResultsSignal = new MockWritableSignal<SearchResponse | null>(null) as WritableSignal<SearchResponse | null>;
+
         mockSearchDataService = {
-            searchResults$: signal<SearchResponse | null>(null),
+            searchResults$: searchResultsSignal,
         };
 
         TestBed.configureTestingModule({
@@ -38,14 +64,10 @@ describe("SearchResultsComponent", () => {
             items: [],
         };
 
-        if (mockSearchDataService.searchResults$) {
-            mockSearchDataService.searchResults$.set(searchResponse);
-            fixture.detectChanges();
+        mockSearchDataService.searchResults$!.set(searchResponse);
+        fixture.detectChanges();
 
-            const searchResults = mockSearchDataService.searchResults$();
-            expect(searchResults?.items.length).toBe(0);
-        } else {
-            fail("mockSearchDataService.searchResults$ is undefined");
-        }
+        const searchResults = mockSearchDataService.searchResults$!();
+        expect(searchResults?.items.length).toBe(0);
     });
 });
